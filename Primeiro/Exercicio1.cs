d34f72961d1beb0f9b54aadaf905e5a5059e53dc
paullohenriquecamargo <49409587+paullohenriquecamargo@users.noreply.github.com>
2020-01-21 13:59:43 -0300

    Exercicio 1 (Seção 3/Aula 18).

---
+++ Primeiro/Exercicio1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Globalization;
+
+namespace Primeiro
+{
+	class Exercicio1
+	{
+		static void Main(string[] args)
+		{
+			string produto1 = "Computador";
+			string produto2 = "Mesa de escritório";
+
+			byte idade = 30;
+			int codigo = 5290;
+			char genero = 'M';
+
+			double preco1 = 2100.0;
+			double preco2 = 650.50;
+			double medida = 53.234567;
+
+			Console.WriteLine($"Produtos: {produto1}, {produto2}");
+			Console.WriteLine($"{produto1}, cujo preço é $2100,00 " +
+							  $"{produto2}, cujo preço é $650,50");
+
+			Console.WriteLine("Registro: "+ idade + " código é " + codigo + " e gênero: " + genero);
+			Console.WriteLine($"Medida com oito casas decimais :{medida}00");
+			Console.WriteLine($"Arredondado: {medida.ToString("F3")}");
+			Console.WriteLine($"Separador decimal invariant culture: {medida.ToString("F3", CultureInfo.InvariantCulture)}");
+		}
+	}
+}
