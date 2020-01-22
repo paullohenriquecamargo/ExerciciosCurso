26be06d811d1b8fa3d9b96c08b67903568a429a1
paullohenriquecamargo <49409587+paullohenriquecamargo@users.noreply.github.com>
2020-01-22 17:19:16 -0300

    Exercicio2(Entradada de Dados)

--- Exercicio2/Exercicio2.cs
+++ Exercicio2/Exercicio2.cs
@@ -6,7 +6,19 @@ namespace Exercicio2
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			string nome = Console.ReadLine();
+			Console.WriteLine($"Digite seu nome completo: {nome}");
+
+			int numeroQuartos = int.Parse(Console.ReadLine());
+			Console.WriteLine($"Quantos quartos tem na sua casa ?: {numeroQuartos}");
+
+			double preco = double.Parse(Console.ReadLine());
+			Console.WriteLine($"Entre com o preço de um produto: {preco}");
+
+			string[] vet = Console.ReadLine().Split(' ');
+			string ultimoNome = vet[0];
+			int idade = int.Parse(vet[0]);
+			double altura = double.Parse(vet[1]);
 		}
 	}
 }
