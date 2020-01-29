e334f398c51f294a5174272067ee0ee3b8fc698e
paullohenriquecamargo <49409587+paullohenriquecamargo@users.noreply.github.com>
2020-01-29 15:20:14 -0300

    ExercicioMembrosEstaticos.

---
+++ MembrosEstaticos/ConversorDeMoeda.cs
@@ -0,0 +1,24 @@
+﻿namespace MembrosEstaticos
+{
+	class ConversorDeMoeda
+	{
+		public double Dolar { get; set; }
+		public double Cotagem { get; set; }
+		public double Iof { get; set; } = 1.06;
+		public double Reais { get; set; }
+		public double TotalEmReais { get; set; }
+		public double DolarFinal { get; set; }
+
+		
+
+		public double Converter()
+		{
+			return DolarFinal = Dolar * Iof;
+		}
+				
+		public double ValorReais()
+		{
+			return TotalEmReais = DolarFinal * Cotagem;
+		}
+	}
+}
---
+++ MembrosEstaticos/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace MembrosEstaticos
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			ConversorDeMoeda conversor = new ConversorDeMoeda();
+			Console.Write("Digite a cotagem do Dolar: ");
+			conversor.Cotagem = double.Parse(Console.ReadLine());
+
+			Console.WriteLine("Quantos dolares você vai comprar: ");
+			conversor.Dolar = double.Parse(Console.ReadLine());
+
+			conversor.Converter();
+
+			Console.WriteLine($"Valor total em reais: {conversor.ValorReais().ToString("F2")}");
+		}
+	}
+}
