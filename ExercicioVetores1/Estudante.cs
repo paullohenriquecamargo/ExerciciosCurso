54a63a0fb0bd13dde25ccebfeacb80e1afa723d6
paullohenriquecamargo <49409587+paullohenriquecamargo@users.noreply.github.com>
2020-01-29 18:42:09 -0300

    ExercicioVetores.

---
+++ ExercicioVetores1/Estudante.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExercicioVetores1
+{
+	class Estudante
+	{
+		public string Nome;
+		public string Email;
+
+		public Estudante(string nome, string email)
+		{
+			Nome = nome;
+			Email = email;
+		}
+
+		public string PlaceHolder()
+		{
+			return Nome + " , " + Email;
+		}
+	}
+}
---
+++ ExercicioVetores1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace ExercicioVetores1
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Estudante[] estudantes = new Estudante[10];
+
+			Console.WriteLine("Informe o número de quartos: ");
+			int numeroQuartos = int.Parse(Console.ReadLine());
+
+			for(int i = 0; i < numeroQuartos; i++)
+			{
+				Console.Write("Digite o nome do estudante: ");
+				string nome = Console.ReadLine();
+
+				Console.Write("Digite o email do estudante: ");
+				string email = Console.ReadLine();
+
+				Console.Write("Digite qual quarto voce vai querer ocupar: ");
+				int quarto = int.Parse(Console.ReadLine());
+
+				Console.Clear();
+				estudantes[quarto] = new Estudante(nome, email);
+			}
+
+			for(int i = 0; i < 10; i++)
+			{
+				var quartosDisponiveis = "";
+				if (estudantes[i] != null)
+					quartosDisponiveis = "Reservado";
+				else
+					quartosDisponiveis = "Disponível";
+
+				Console.WriteLine($"Quartos disponiveis: {quartosDisponiveis}");
+			}
+		}
+	}
+}
