05ea3bd1dc0db5159e75733d77b9da4a97324f03
paullohenriquecamargo <49409587+paullohenriquecamargo@users.noreply.github.com>
2020-03-06 10:52:19 -0300

    Exercicio Resolvido(Enumerações e composição).

---
+++ ExercicioProposto/Entities/Client.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ExercicioProposto.Entities
+{
+	public class Client
+	{
+		public string Name { get; set; }
+		public string Email { get; set; }
+		public DateTime BirthDate { get; set; }
+
+		public Client()
+		{
+		}
+
+		public Client(string name, string email, DateTime birhDate)
+		{
+			Name = name;
+			Email = email;
+			BirthDate = birhDate;
+		}
+
+		public override string ToString()
+		{
+			return Name
+				+ ", ("
+				+ BirthDate.ToString("dd/MM/yyyy")
+				+ ") - "
+				+ Email;
+		}
+	}
+}
---
+++ ExercicioProposto/Entities/OrderStatus.cs
@@ -0,0 +1,10 @@
+﻿namespace ExercicioProposto.Entities
+{
+	public enum OrderStatus : int 
+	{
+		PendingPayment = 0,
+		Processing = 1,
+		Shipped = 2,
+		Delivered = 3
+	}
+}
